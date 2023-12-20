634f3ad988ca6d61eec40501deffe81de6d6566b
ar1619 <andrianirina.rakotoharisoa19@imperial.ac.uk>
2023-12-20 13:20:50 +0000

    added for 1 GPU

---
+++ hp_tuning_1gpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=hp_tuning
+#SBATCH -e hp_tun-%j.err
+#SBATCH -o hp_tun-%j.out
+#SBATCH --mem-per-cpu=1500
+#SBATCH --time=4-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+
+#nvidia-smi
+#nvcc --version
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate gpu_current2
+
+srun python -u hyperparameter_tuning.py --start_iter 1 --patch_size 32 --gpus 1 --hr_train_dataset ../MOD_tensor
