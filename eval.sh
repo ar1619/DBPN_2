614f0a7a72e43c0135ece2eee4e1d8f2f02ea0f7
ar1619 <andrianirina.rakotoharisoa19@imperial.ac.uk>
2024-09-27 10:26:53 +0000

    adapted slurm file to new cluster

--- eval.sh
+++ eval.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=cent_arr_sr
+#SBATCH --job-name=sr_16
+#SBATCH --partition=hive_flash
+#SBATCH --qos=hive_flash_low
+
 #SBATCH -e sr_w_noise-%j.err
 #SBATCH -o sr_w_noise-%j.out
 #SBATCH --mem-per-cpu=1500
@@ -11,9 +14,9 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --gres=gpu:1
 
-#nvidia-smi
+nvidia-smi
 #nvcc --version
-source ~/anaconda3/etc/profile.d/conda.sh
+source /scratch_hive/ar1619/miniconda3/etc/profile.d/conda.sh
 conda activate gpu_current2
 
-srun python eval.py --upscale_factor 16 --combination 2 --input_dir Input --output Result --test_dataset ICCS --model weights/new_loss_post_tuning.pth
+srun python eval.py --upscale_factor 16 --combination 2 --input_dir Input --output Result --test_dataset sentinel --model weights/new_loss_post_tuning.pth
